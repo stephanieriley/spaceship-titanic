981f70bb65a1c276981d67d0e3d5e3fa5414e365
stephanieriley <stephanieriley333@gmail.com>
2023-06-12 22:38:10 +0100

    populate model build tab

--- R/spaceship-titanic/ui.R
+++ R/spaceship-titanic/ui.R
@@ -32,7 +32,7 @@ body<- dashboardBody(
     #Welcome tab
     tabItem(tabName = "intro", h1("Welcome aboard the Spaceship Titanic!"),
             p("Welcome to the year 2912, where your data science skills are needed to solve a cosmic mystery. We've received a transmission from four lightyears away and things aren't looking good."),
-            p("The Spaceship Titanic was an interstellar passenger liner launched a month ago. With almost 13,000 passengers on board, the vessel set out on its maiden voyage transporting emigrants from our solar system to three newly habitable exoplanets orbiting nearby stars."),
+            p("The Spaceship Titanic was an interstellar passenger liner launched a month ago. With almost 9,000 passengers on board, the vessel set out on its maiden voyage transporting emigrants from our solar system to three newly habitable exoplanets orbiting nearby stars."),
             p("While rounding Alpha Centauri en route to its first destination—the torrid 55 Cancri E—the unwary Spaceship Titanic collided with a spacetime anomaly hidden within a dust cloud. Sadly, it met a similar fate as its namesake from 1000 years before. Though the ship stayed intact, more than half of the passengers were transported to an alternate dimension!"),
             br(),
             p("Today we will attempt to build a statistical model which can predict whether someone will be ", strong("Transported"), " or ", strong("Not Transported"), " off the Spaceship Titanic!"),
@@ -49,7 +49,20 @@ body<- dashboardBody(
                         choices = c("Categorical predictors" = "cat",
                                     "Continuous predictors" = "cont")),
             mainPanel(plotOutput("summaryplot",
-                                 height = 700)))
+                                 height = 700))),
+    
+    #Build the model
+    #First select predictor variables
+    tabItem(tabName = "modelbuild", h1("Build your model"),
+            p("Select which variables you would like to include in the prediction model"),
+            selectInput("preds", "Predictor variables:",
+                        c("Age", "RoomService", "FoodCourt", "ShoppingMall", "Spa", "VRDeck", "HomePlanet", "Destination", "DeckCat", "Side", "VIP", "CryoSleep"),
+                        multiple = TRUE),
+            actionButton("build", "Build model"),
+            p("Summary of the model"),
+            verbatimTextOutput("modelsum"))
+    
+    
   )
 )
 
