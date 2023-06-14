d5eefae6b8e55913eba98e45c9ac2400052f9a87
stephanieriley <stephanieriley333@gmail.com>
2023-06-14 15:28:51 +0100

    add null model for when no predictors selected. calculate model performance.

--- R/spaceship-titanic/server.R
+++ R/spaceship-titanic/server.R
@@ -13,9 +13,6 @@ library(gtsummary)
 library(gt)
 library(ggpubr)
 library(survival)
-library(pROC)
-
-# dat <- read.csv("data/data.csv")
 
 
 
@@ -90,30 +87,55 @@ output$summaryplot<- renderPlot ({
 
 # Define formula to be used in glm
 form<- reactive({
-  as.formula(paste0("Transported ~ ", paste(input$preds, collapse = "+")))
+  if(is.null(input$preds)) {
+    as.formula("Transported ~ 1")
+  } else {
+    as.formula(paste0("Transported ~ ", paste(input$preds, collapse = "+")))
+  }
 })
 
 
 # Run glm
-model<- eventReactive(input$build, {
+model<- reactive({
   glm(form(), data = data(), family = binomial)
 })
   
 
 
-# Model summary
-output$modelsum<- renderPrint(summary(model()))
-
-
-# Model discrimination using concordance stat
-output$disc<- concordance(model())
-
-
-# # Model AUC
-# predictedprobs<- reactive({
-#   
+# # Model summary
+# #Only find model summary once action button is clicked
+# modelsumm<- eventReactive(input$build, {
+#   summary(model())
 # })
-# output$auc<- 
+# 
+# output$modelsum<- renderPrint(modelsumm())
+  
+
+
+# Model discrimination using concordance stat (equivalent to AUC)
+disc<- eventReactive(input$build, {
+  as.numeric(concordance(model())$concordance) %>% round(digits=3)
+})
+#infobox
+output$discBox<- renderInfoBox({
+  infoBox(
+    "Discrimination", renderText(paste0(disc()*100, "%")), icon = icon("percent")
+  )
+})
+
+# McFadden's R-squared
+#Need loglikelihood for null model
+nullmod<- reactive({glm(Transported ~ 1, data = data(), family = binomial)})
+#Calculate R-squared
+rsquared<- eventReactive(input$build, {
+  1 - (logLik(model()) / logLik(nullmod())) %>% round(digits=4)
+}) 
+#infobox
+output$rsquaredBox<- renderInfoBox({
+  infoBox("R-squared", renderText(paste0(rsquared()*100, "%")), icon = icon("percent"))
+})
+
+
 
 }
 
